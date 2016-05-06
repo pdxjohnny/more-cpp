085f2ef200414d7343ca8792436a052616e76f20
John Andersen <johnandersenpdx@gmail.com>
2016-05-06 06:21:36 +0000

    Tried to add multiplayer but it didnt work
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- programs/cards/src/cards.cpp
+++ programs/cards/src/cards.cpp
@@ -9,10 +9,24 @@
 int main(int argc, char ** argv, char ** env) {
     // The first agrument should be the name of the game we want to play
     char * game_name = NULL;
+    bool mp = false;
     if (argc < 2) {
         errno = EINVAL;
-        MACRO_LOG_FATAL("Usage: %s name_of_card_game", argv[0]);
+        MACRO_LOG_FATAL("Usage: %s name_of_card_game mp", argv[0]);
     }
+    /*
+    if (argc > 2) {
+        mp = true;
+        std::cout << "By inputing three arguments you have selected" <<
+            " multiplayer mode. This means everytime you take an action" <<
+            " the next action will be taken by the other player. There was" <<
+            " not enough time for sockets so sorry its splitscreen!" <<
+            " (more like share screen) Press enter to continue" << std::endl;
+        const int buffer_size = 100;
+        char buffer[buffer_size];
+        std::cin.getline(buffer, buffer_size);
+    }
+    */
     game_name = argv[1];
     // Ask cards to provide us with an instance of that game
     cards::game * game = cards::game_from_string(game_name);
@@ -21,7 +35,12 @@ int main(int argc, char ** argv, char ** env) {
     }
     // Create a basic player
     cards::player player_one(&std::cout, &std::cin);
-    game->play(1, &player_one);
+    cards::player player_two(&std::cout, &std::cin);
+    if (mp == true) {
+        game->play(2, &player_one, &player_two);
+    } else {
+        game->play(1, &player_one);
+    }
     try {
         // We dont need to do anything in between turns
         while(game->next_turn()) {};
